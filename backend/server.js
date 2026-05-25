e8d17a6249e22b14a3b73c3166dd23aa40f5a6ea
Lucas Vieira <lucasvieiramoura@gmail.com>
2026-05-25 09:54:49 -0300

    Criando conexão com o banco de dados Mongo e subindo o servidor

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+//Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
+
+//Conecta ao banco de dados MongoDB
+connectDB();
+
+const app = express();
+
+//Middleware para habilitar CORS e parsear JSON
+app.use(cors());
+app.use(express.json()); // Permite que o Express entenda requisições com corpo JSON no corpo da requisição
+
+//Rotas
+app.use('/', (req, res) => {
+  res.send('API ToDo List funcionando!');
+}   );
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
